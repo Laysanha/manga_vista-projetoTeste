f0de07742f1a2043e4bf1e1d2c7a5c8f577594b2
Laysa Santiago Alfredo <laysatb78@gmail.com>
2024-05-16 15:31:11 -0300

    feat(Adicionar menu Bar no aplicativo):
    - Adiconado menu bar no aplicativo;
    - Adicionado paginas referentes as opçoes do menu;
    - Adiconado App bar inical;

---
+++ lib/pages/page_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class PageHome extends StatefulWidget {
+  const PageHome({super.key});
+
+  @override
+  State<PageHome> createState() => _PageHomeState();
+}
+
+class _PageHomeState extends State<PageHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Home"));
+  }
+}
